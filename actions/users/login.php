4882cd7d549f980ea4c45302c5c9d132cf7f9b91
ZPaulo <jose.p.s.olive@gmail.com>
2015-04-25 19:32:22 +0100

    login done

--- actions/users/login.php
+++ actions/users/login.php
@@ -1,14 +1,12 @@
 <?php
   include_once('../../config/init.php');
   include_once($BASE_DIR .'database/users.php');  
-  include 'ChromePhp.php';
 
 
   if (!$_POST['username'] || !$_POST['password']) {
     $_SESSION['error_messages'][] = 'Invalid login';
     $_SESSION['form_values'] = $_POST;
-    echo "<script type='text/javascript'>alert('invalid');</script>";
-    //header('Location: ' . $_SERVER['HTTP_REFERER']);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
     exit;
   }
 
@@ -17,11 +15,9 @@
   
   if (isLoginCorrect($username, $password)) {
     $_SESSION['username'] = $username;
-    echo "<script type='text/javascript'>alert('okidoki');</script>";
     $_SESSION['success_messages'][] = 'Login successful';  
   } else {
-    ChromePhp::warn('something went wrong!');
     $_SESSION['error_messages'][] = 'Login failed';  
   }
-  //header('Location: ' . $_SERVER['HTTP_REFERER']);
+  header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
