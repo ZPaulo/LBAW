ab260ed9520d543907c9b89bd66bb03d4157b633
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-04-26 21:58:39 +0100

    asdads

---
+++ pages/footer.php
@@ -0,0 +1,95 @@
+	<div class="footer-v1">
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<!-- About -->
+					<div class="col-md-3 md-margin-bottom-40">
+						<a href="index-2.html"><img id="logo-footer"
+							class="footer-logo" src="assets/img/logo2.png" alt=""></a>
+						<p>O minja é o pior Nasus e consegue feedar em todos os
+							jogos. O Zé é sombrio e isso faz com que ele não saia de silver.</p>
+					</div>
+					<!--/col-md-3-->
+					<!-- End About -->
+
+					<!-- Link List -->
+					<div class="col-md-3 md-margin-bottom-40">
+						<div class="headline">
+							<h2>Useful Links</h2>
+						</div>
+						<ul class="list-unstyled link-list">
+							<li><a href="#">About us</a><i class="fa fa-angle-right"></i></li>
+							<li><a href="#">FAQ</a><i class="fa fa-angle-right"></i></li>
+							<li><a href="#">Contact us</a><i class="fa fa-angle-right"></i></li>
+						</ul>
+					</div>
+					<!--/col-md-3-->
+					<!-- End Link List -->
+
+					<!-- Address -->
+					<div class="col-md-3 map-img md-margin-bottom-40">
+						<div class="headline">
+							<h2>Contact Us</h2>
+						</div>
+						<address class="md-margin-bottom-40">
+							Rua dos feeders <br /> Porto Portugal <br /> Phone: f33d f33d
+							f33d <br /> Email: <a href="" class="">feeders@feeders.com</a>
+						</address>
+					</div>
+					<!--/col-md-3-->
+					<!-- End Address -->
+				</div>
+			</div>
+		</div>
+		<!--/footer-->
+
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<p>
+							2015 &copy; All Rights Reserved. <a href="#">Privacy Policy</a>
+							| <a href="#">Terms of Service</a>
+						</p>
+					</div>
+
+					<!-- Social Links -->
+					<div class="col-md-6">
+						<ul class="footer-socials list-inline">
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Facebook">
+									<i class="fa fa-facebook"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Skype">
+									<i class="fa fa-skype"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Google Plus">
+									<i class="fa fa-google-plus"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Linkedin">
+									<i class="fa fa-linkedin"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Pinterest">
+									<i class="fa fa-pinterest"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Twitter">
+									<i class="fa fa-twitter"></i>
+							</a></li>
+							<li><a href="#" class="tooltips" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="Dribbble">
+									<i class="fa fa-dribbble"></i>
+							</a></li>
+						</ul>
+					</div>
+					<!-- End Social Links -->
+				</div>
+			</div>
+		</div>
+		<!--/copyright-->
+	</div>
+</div>
