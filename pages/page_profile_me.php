09456c57ca8cd1b175397afef34513cafe7a8e65
leonardo-faria <leonardofaria94@gmail.com>
2015-04-26 21:25:50 +0100

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

---
+++ pages/page_profile_me.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<!-- Mirrored from htmlstream.com/preview/unify-v1.7/page_profile_me.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 16 Mar 2015 12:32:43 GMT -->
+<head>
+<title>Profile Me | Unify - Responsive Website Template</title>
+
+<!-- Meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="favicon.html">
+
+<!-- Web Fonts -->
+<link rel='stylesheet' type='text/css'
+	href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600&amp;subset=cyrillic,latin'>
+
+<!-- CSS Global Compulsory -->
+<link rel="stylesheet"
+	href="assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+
+<!-- CSS Header and Footer -->
+<link rel="stylesheet" href="assets/css/headers/header-default.css">
+<link rel="stylesheet" href="assets/css/footers/footer-v1.css">
+
+<!-- CSS Implementing Plugins -->
+<link rel="stylesheet" href="assets/plugins/animate.css">
+<link rel="stylesheet" href="assets/plugins/line-icons/line-icons.css">
+<link rel="stylesheet"
+	href="assets/plugins/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet"
+	href="assets/plugins/scrollbar/css/jquery.mCustomScrollbar.css">
+<link rel="stylesheet"
+	href="assets/plugins/sky-forms-pro/skyforms/css/sky-forms.css">
+<link rel="stylesheet"
+	href="assets/plugins/sky-forms-pro/skyforms/custom/custom-sky-forms.css">
+
+<!-- CSS Page Style -->
+<link rel="stylesheet" href="assets/css/pages/profile.css">
+<link rel="stylesheet" href="assets/css/pages/shortcode_timeline2.css">
+
+<!-- CSS Theme -->
+<link rel="stylesheet" href="assets/css/theme-colors/default.css"
+	id="style_color">
+<link rel="stylesheet" href="assets/css/theme-skins/dark.css">
+
+<!-- CSS Customization -->
+<link rel="stylesheet" href="assets/css/custom.css">
+</head>
+
+<body>
+
+	<div class="wrapper">
+		<!--=== Header ===-->
+		<div class="header">
+			<div class="container">
+				<!-- Logo -->
+				<a class="logo" href="index.html"> <img
+					src="assets/img/logo2.png" alt="Logo">
+				</a>
+				<!-- End Logo -->
+				<!-- Topbar -->
+				<div class="topbar">
+					<ul class="loginbar pull-right">
+						<li class="hoverSelector"><i class="fa fa-globe"></i> <a>Languages</a>
+							<ul class="languages hoverSelectorBlock">
+								<li class="active"><a href="#">English <i
+										class="fa fa-check"></i></a></li>
+								<li><a href="#">Spanish</a></li>
+								<li><a href="#">Russian</a></li>
+								<li><a href="#">German</a></li>
+							</ul></li>
+						<li class="topbar-devider"></li>
+						<li><a href="page_login.html">Login</a></li>
+					</ul>
+				</div>
+				<!-- End Topbar -->
+
+				<!-- Toggle get grouped for better mobile display -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-responsive-collapse">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="fa fa-bars"></span>
+				</button>
+				<!-- End Toggle -->
+			</div>
+			<!--/end container-->
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div
+				class="collapse navbar-collapse mega-menu navbar-responsive-collapse">
+				<div class="container">
+					<ul class="nav navbar-nav">
+						<!-- Home -->
+						<li class=""><a href="index.html" class="" data-toggle="">
+								Home </a></li>
+						<li class=""><a href="car_search.html" class=""
+							data-toggle=""> Fleet </a></li>
+						<!-- End Home -->
+
+						<!-- Pages -->
+						<li class="dropdown active"><a href="javascript:void(0);"
+							class="dropdown-toggle" data-toggle="dropdown"> Profile </a>
+							<ul class="dropdown-menu">
+								<li class="active"><a href="page_profile_me.html">Overview</a></li>
+								<li><a href="page_profile_history.html">History</a></li>
+								<li><a href="page_profile_settings.html">Settings</a></li>
+							</ul></li>
+						<li class="dropdown"><a href="javascript:void(0);"
+							class="dropdown-toggle" data-toggle="dropdown"
+							onclick="window.location = 'management.html'"> Management </a>
+							<ul class="dropdown-menu">
+								<li><a href="create_local.html">Create Location</a></li>
+								<li><a href="create_vehicle.html">Create Vehicle</a></li>
+								<li><a href="car_search.html">Search Vehicles</a></li>
+								<li><a href="user_search.html">Search Users</a></li>
+							</ul></li>
+						<li class="dropdown "><a href="javascript:void(0);"
+							class="dropdown-toggle" data-toggle="dropdown"
+							onclick="window.location = 'administration.html'">
+								Administration </a>
+							<ul class="dropdown-menu">
+								<li><a href="create_manager.html">Create Manager</a></li>
+								<li><a href="search_manager.html">Search Managers</a></li>
+							</ul></li>
+						<li><i class="search fa fa-search search-btn"></i>
+							<div class="search-open">
+								<div class="input-group animated fadeInDown">
+									<input type="text" class="form-control" placeholder="Search">
+									<span class="input-group-btn">
+										<button class="btn-u" type="button">Go</button>
+									</span>
+								</div>
+							</div></li>
+						<!-- End Search Block -->
+					</ul>
+				</div>
+				<!--/end container-->
+			</div>
+			<!--/navbar-collapse-->
+		</div>
+		<!--=== End Header ===-->
+
+		<!--=== Profile ===-->
+		<div class="container content profile">
+			<div class="row">
+				<!--Left Sidebar-->
+				<div class="col-md-3 md-margin-bottom-40">
+
+					<ul class="list-group sidebar-nav-v1 margin-bottom-40"
+						id="sidebar-nav-1">
+						<li class="list-group-item active"><a
+							href="page_profile_me.html"><i class="fa fa-user"></i>
+								Profile</a></li>
+						<li class="list-group-item"><a
+							href="page_profile_history.html"><i class="fa fa-history"></i>
+								History</a></li>
+						<li class="list-group-item"><a
+							href="page_profile_settings.html"><i class="fa fa-cog"></i>
+								Settings</a></li>
+					</ul>
+
+					<!--End Notification-->
+
+
+					<!--Datepicker-->
+					<form action="#" id="sky-form2" class="sky-form">
+						<div id="inline-start"></div>
+					</form>
+					<!--End Datepicker-->
+				</div>
+				<!--End Left Sidebar-->
+
+				<!-- Profile Content -->
+				<div class="col-md-9">
+					<div class="profile-body">
+						<div class="profile-bio">
+							<div class="row">
+								<div class="col-md-5">
+									<img class="img-responsive md-margin-bottom-10"
+										src="assets/img/team/img1-md.jpg" alt=""> <a
+										class="btn-u btn-u-sm" href="#">Change Picture</a>
+								</div>
+								<div class="col-md-7">
+									<h2>Edward Rooster</h2>
+									<span><strong>Address:</strong> USA</span> <span><strong>Age:</strong>
+										69</span>
+									<hr>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Suspendisse eget massa nec turpis congue bibendum. Integer
+										nulla felis, porta suscipit nulla et, dignissim commodo nunc.
+										Morbi a semper nulla.</p>
+									<p>Proin mauris odio, pharetra quis ligula non, vulputate
+										vehicula quam. Nunc in libero vitae nunc ultricies tincidunt
+										ut sed leo. Sed luctus dui ut congue consequat. Cras consequat
+										nisl ante, nec malesuada velit pellentesque ac. Pellentesque
+										nec arcu in ipsum iaculis convallis.</p>
+								</div>
+							</div>
+						</div>
+						<!--/end row-->
+
+						<hr>
+
+						<div class="row">
+							<!--Social Icons v3-->
+							<div class="col-sm-6 sm-margin-bottom-30">
+								<div class="panel panel-profile">
+									<div class="panel-heading overflow-h">
+										<h2 class="panel-title heading-sm pull-left">
+											<i class="fa fa-pencil"></i> Social Contacts
+										</h2>
+										<a href="#"><i class="fa fa-cog pull-right"></i></a>
+									</div>
+									<div class="panel-body">
+										<ul class="list-unstyled social-contacts-v2">
+											<li><i class="rounded-x tw fa fa-twitter"></i> <a
+												href="#">edward.rooster</a></li>
+											<li><i class="rounded-x fb fa fa-facebook"></i> <a
+												href="#">Edward Rooster</a></li>
+											<li><i class="rounded-x sk fa fa-skype"></i> <a href="#">edwardRooster77</a></li>
+											<li><i class="rounded-x gp fa fa-google-plus"></i> <a
+												href="#">rooster77edward</a></li>
+											<li><i class="rounded-x gm fa fa-envelope"></i> <a
+												href="#">edward77@gmail.com</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!--End Social Icons v3-->
+						</div>
+						<!--/end row-->
+
+
+
+						<!--End Timeline-->
+
+						<!--/end row-->
+					</div>
+				</div>
+				<!-- End Profile Content -->
+			</div>
+		</div>
+		<!--=== End Profile ===-->
+
+		<!--=== Footer Version 1 ===-->
+		<div class="footer-v1">
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <!-- About -->
+                    <div class="col-md-3 md-margin-bottom-40">
+                        <a href="index-2.html"><img id="logo-footer" class="footer-logo" src="assets/img/logo2.png" alt=""></a>
+                        <p>O minja é o pior Nasus e consegue feedar em todos os jogos. O Zé é sombrio e isso faz com que ele não saia de silver.</p>
+                    </div><!--/col-md-3-->
+                    <!-- End About -->
+
+                    <!-- Link List -->
+                    <div class="col-md-3 md-margin-bottom-40">
+                        <div class="headline"><h2>Useful Links</h2></div>
+                        <ul class="list-unstyled link-list">
+                            <li><a href="#">About us</a><i class="fa fa-angle-right"></i></li>
+                            <li><a href="#">FAQ</a><i class="fa fa-angle-right"></i></li>
+                            <li><a href="#">Contact us</a><i class="fa fa-angle-right"></i></li>
+                        </ul>
+                    </div><!--/col-md-3-->
+                    <!-- End Link List -->                    
+
+                    <!-- Address -->
+                    <div class="col-md-3 map-img md-margin-bottom-40">
+                        <div class="headline"><h2>Contact Us</h2></div>                         
+                        <address class="md-margin-bottom-40">
+                            Rua dos feeders <br />
+                            Porto Portugal <br />
+                            Phone: f33d f33d f33d <br />
+                            Email: <a href="" class="">feeders@feeders.com</a>
+                        </address>
+                    </div><!--/col-md-3-->
+                    <!-- End Address -->
+                </div>
+            </div> 
+        </div><!--/footer-->
+
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">                     
+                        <p>
+                            2015 &copy; All Rights Reserved.
+                           <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+                        </p>
+                    </div>
+
+                    <!-- Social Links -->
+                    <div class="col-md-6">
+                        <ul class="footer-socials list-inline">
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Skype">
+                                    <i class="fa fa-skype"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Linkedin">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest">
+                                    <i class="fa fa-pinterest"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Dribbble">
+                                    <i class="fa fa-dribbble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- End Social Links -->
+                </div>
+            </div> 
+        </div><!--/copyright-->
+    </div>
+		<!--=== End Footer Version 1 ===-->
+	</div>
+	<!--/wrapper-->
+
+	<!-- JS Global Compulsory -->
+	<script type="text/javascript"
+		src="assets/plugins/jquery/jquery.min.js"></script>
+	<script type="text/javascript"
+		src="assets/plugins/jquery/jquery-migrate.min.js"></script>
+	<script type="text/javascript"
+		src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<!-- JS Implementing Plugins -->
+	<script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+	<script type="text/javascript" src="assets/plugins/smoothScroll.js"></script>
+	<script type="text/javascript"
+		src="assets/plugins/circles-master/circles.js"></script>
+	<script type="text/javascript"
+		src="assets/plugins/sky-forms-pro/skyforms/js/jquery-ui.min.js"></script>
+	<script type="text/javascript"
+		src="assets/plugins/scrollbar/js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<!-- JS Customization -->
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+	<!-- JS Page Level -->
+	<script type="text/javascript" src="assets/js/app.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/datepicker.js"></script>
+	<script type="text/javascript"
+		src="assets/js/plugins/circles-master.js"></script>
+	<script type="text/javascript"
+		src="assets/js/plugins/style-switcher.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+			App.init();
+			App.initScrollBar();
+			Datepicker.initDatepicker();
+			CirclesMaster.initCirclesMaster1();
+			StyleSwitcher.initStyleSwitcher();
+		});
+	</script>
+	<!--[if lt IE 9]>
+    <script src="assets/plugins/respond.js"></script>
+    <script src="assets/plugins/html5shiv.js"></script>
+    <script src="assets/plugins/placeholder-IE-fixes.js"></script>
+<![endif]-->
+
+</body>
+
+<!-- Mirrored from htmlstream.com/preview/unify-v1.7/page_profile_me.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 16 Mar 2015 12:32:44 GMT -->
+</html>
