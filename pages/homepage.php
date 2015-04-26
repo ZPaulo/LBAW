58a2d8b13618bfce405f9e276c862825bf747373
ZPaulo <jose.p.s.olive@gmail.com>
2015-04-26 17:59:45 +0100

    s

--- pages/homepage.php
+++ pages/homepage.php
@@ -75,11 +75,13 @@
 								<li><a href="#">German</a></li>
 							</ul></li>
 						<li class="topbar-devider"></li>
-						<li><?php if(isset($_SESSION['username'])
+						<li><?php if(isset($_SESSION['username']){
 									echo '<a href="page_login.html">'.$_SESSION['username'].'</a>)'; 
-									else 
+									}
+									else{
 										echo '<a href="page_login.html">Login</a>)';
-										</li> ?>
+									}
+										?></li> 
 						<li class="topbar-devider"></li>
 						<li><a href="page_registration.html">Register</a></li>
 					</ul>
