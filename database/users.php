25078c184ec372913fb828c5acc836c5eba88354
ZPaulo <jose.p.s.olive@gmail.com>
2015-04-26 16:41:06 +0100

    s

--- database/users.php
+++ database/users.php
@@ -2,21 +2,16 @@
   
   function createUser($realname, $username, $password, $email, $cellphone, $adress) {
     global $conn;
-    echo $realname;
-    echo $username;
-    echo $password;
-    echo $email;
-    echo $cellphone;
-    echo $adress;/*
+   /*
     $stmt = $conn->prepare("INSERT INTO pessoa VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->execute(array($realname, $username, sha1($password), $email, $cellphone, $adress));
-
+*/
     $stmt = $conn->prepare("SELECT pessoaid FROM pessoa WHERE  username = ?");
     $stmt->execute(array($username));
     $id = $stmt->fetch();
 
     $stmt = $conn->prepare("INSERT INTO cliente VALUES (?, ?)");
-    $stmt->execute(array(NULL,$id);*/
+    $stmt->execute(array(NULL,$id);
   }
 
   function isLoginCorrect($username, $password) {
