b5f8d98d062a4b4935c43de310ed527913bf5530
leonardo-faria <leonardofaria94@gmail.com>
2015-05-11 00:10:50 +0100

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- actions/local/create_local.php
+++ actions/local/create_local.php
@@ -3,21 +3,19 @@ include_once ('../../config/init.php');
 include_once ($BASE_DIR . 'database/local.php');
 session_start ();
 
-$name = $_GET ['username'];
-$map = $_GET ['password'];
+$name = $_POST ['name'];
+$map = $_POST ['map'];
 
-if (isClientCorrect ( $username, $password )) {
-	
-	$_SESSION ['username'] = $username;
-	$_SESSION ['type'] = 'client';
-	$_SESSION ['success_messages'] [] = 'Login successful';
-} else if (isAdminCorrect ( $username, $password )) {
-	$_SESSION ['username'] = $username;
-	$_SESSION ['type'] = 'admin';
-	$_SESSION ['success_messages'] [] = 'Login successful';
-} else {
-	$_SESSION ['error_messages'] [] = 'Login failed';
+try {
+	createLocal ( $name, $map );
+} catch ( PDOException $e ) {
+    $_SESSION['error_messages'][] = 'Error creating local';
+
+    $_SESSION['form_values'] = $_POST;
+    header("Location: $BASE_URL");
+    exit;
 }
+
 header ( 'Location: ' . $_SERVER ['HTTP_REFERER'] );
 // header('Location: ' . $BASE_URL);
 ?>
---
+++ database/local.php
@@ -0,0 +1,18 @@
+<?php
+  function createLocal($name, $map) {
+    global $conn;
+  
+    $stmt = $conn->prepare("INSERT INTO pessoa VALUES (?, ?)");
+    $stmt->execute(array($name, $map));
+
+  }
+ 
+  function localExists($name, $map) {
+    global $conn;
+    $stmt = $conn->prepare("SELECT * 
+                            FROM localdestand
+                            WHERE morada = ? AND descricao = ?");
+    $stmt->execute(array($name, $map));
+    return $stmt->fetch()==true;
+  }
+?>
