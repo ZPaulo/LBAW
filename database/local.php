b9774b6082bc1169cbd6e1069feb64a72d32db68
leonardo-faria <leonardofaria94@gmail.com>
2015-05-11 00:54:07 +0100

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- database/local.php
+++ database/local.php
@@ -3,7 +3,7 @@
   	echo $name.$map;
     global $conn;
   
-    $stmt = $conn->prepare("INSERT INTO localdestand VALUES (?, ?)");
+    $stmt = $conn->prepare("INSERT INTO localdestand (nome,morada) VALUES (?, ?)");
     $stmt->execute(array($name, $map));
   	echo 'merda2';
 
