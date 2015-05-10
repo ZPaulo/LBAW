ad67435f6991f848c339609098bf1a24fb2b76fa
leonardo-faria <leonardofaria94@gmail.com>
2015-05-11 00:29:50 +0100

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- database/local.php
+++ database/local.php
@@ -1,18 +1,10 @@
 <?php
   function createLocal($name, $map) {
+    header("Location:".$name.$map);
     global $conn;
   
     $stmt = $conn->prepare("INSERT INTO localdestand VALUES (?, ?)");
     $stmt->execute(array($name, $map));
 
-  }
- 
-  function localExists($name, $map) {
-    global $conn;
-    $stmt = $conn->prepare("SELECT * 
-                            FROM localdestand
-                            WHERE morada = ? AND descricao = ?");
-    $stmt->execute(array($name, $map));
-    return $stmt->fetch()==true;
   }
 ?>
