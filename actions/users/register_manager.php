c0775c3f2c996800e7cd2bb0c5fb47e83203e013
leonardo-faria <leonardofaria94@gmail.com>
2015-05-11 02:21:56 +0100

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- actions/users/register_manager.php
+++ actions/users/register_manager.php
@@ -1,37 +1,35 @@
 <?php
-  include_once('../../config/init.php');
-  include_once($BASE_DIR .'database/users.php');  
+include_once ('../../config/init.php');
+include_once ($BASE_DIR . 'database/users.php');
 
-  if (!$_POST['username']  || !$_POST['password'] || !$_POST['local']) {
-    $_SESSION['error_messages'][] = 'All fields are mandatory';
-    $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/create_manager.php');
-    exit;
-  }
+if (! $_POST ['username'] || ! $_POST ['password'] || ! $_POST ['local']) {
+	$_SESSION ['error_messages'] [] = 'All fields are mandatory';
+	$_SESSION ['form_values'] = $_POST;
+	header ( "Location: $BASE_URL" . 'pages/create_manager.php' );
+	exit ();
+}
 
-  $username = strip_tags($_POST['username']);
-  $password = $_POST['password'];
-  $local = $_POST['local'];
-  
-  echo 'local: '.$local;
+$username = strip_tags ( $_POST ['username'] );
+$password = $_POST ['password'];
+$local = $_POST ['local'];
 
-  //$photo = $_FILES['photo'];
-  //$extension = end(explode(".", $photo["name"]));
+// $photo = $_FILES['photo'];
+// $extension = end(explode(".", $photo["name"]));
 
-  try {
-    createManager($username, $password,$local);
-      } catch (PDOException $e) {
-  
-    if (strpos($e->getMessage(), 'users_pkey') !== false) {
-      $_SESSION['error_messages'][] = 'Duplicate username';
-      $_SESSION['field_errors']['username'] = 'Username already exists';
-    }
-    else $_SESSION['error_messages'][] = 'Error creating manager';
-
-    $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/administration.php');
-    exit;
-  }
-  $_SESSION['success_messages'][] = 'Manager registered successfully';  
-  header("Location: ".$_SESSION['HTTP_REFERER']);
+try {
+	createManager ( $username, $password, $local );
+} catch ( PDOException $e ) {
+	
+	if (strpos ( $e->getMessage (), 'users_pkey' ) !== false) {
+		$_SESSION ['error_messages'] [] = 'Duplicate username';
+		$_SESSION ['field_errors'] ['username'] = 'Username already exists';
+	} else
+		$_SESSION ['error_messages'] [] = 'Error creating manager';
+	
+	$_SESSION ['form_values'] = $_POST;
+	header ( "Location: $BASE_URL" . 'pages/administration.php' );
+	exit ();
+}
+$_SESSION ['success_messages'] [] = 'Manager registered successfully';
+header ( "Location: $BASE_URL" . 'pages/administration.php' );
 ?>
